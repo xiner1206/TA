55b71f76fe77447bdc56bae7b507429e14f051d8
FXR <981402130@qq.com>
2019-05-01 00:33:56 +0800

    pictureCORS

--- src/main/java/com/ta/framework/util/CORSFilter.java
+++ src/main/java/com/ta/framework/util/CORSFilter.java
@@ -15,7 +15,7 @@ public class CORSFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         response.addHeader("Access-Control-Allow-Origin", "*");
         response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-        response.addHeader("Access-Control-Allow-Headers", "Content-Type");
+        response.addHeader("Access-Control-Allow-Headers", "*");
         response.addHeader("Access-Control-Max-Age", "1800");//30 min
         filterChain.doFilter(request, response);
     }
